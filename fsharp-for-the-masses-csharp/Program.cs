8cdbee528f50b09aa5d3960906a8142e40c2ea99
npal <nick.palladinos@gmail.com>
2015-03-13 17:34:50 +0200

    Init

---
+++ fsharp-for-the-masses-csharp/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fsharp_for_the_masses
+{
+    class Program
+    {
+
+        #region Example 0
+
+        public class Customer
+        {
+            private readonly string name;
+            private readonly int age;
+            public Customer(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+            public string Name { get { return name; } }
+            public int Age { get { return age; } }
+            #region Equals/HashCode
+            //public override bool Equals(object obj)
+            //{
+            //    /// ...
+            //}
+            //public override int GetHashCode()
+            //{
+            //    /// ...
+            //}
+            #endregion
+        }
+
+        #endregion
+
+        #region Example 1
+        Tuple<U, U, U> Map<T, U>(Func<T, U> f, Tuple<T, T, T> t)
+        {
+            return new Tuple<U, U, U>(f(t.Item1), f(t.Item2), f(t.Item3));
+        }
+        #endregion
+
+        #region Example 2
+
+        public  abstract class Expr 
+        { 
+            public abstract bool Eval(); 
+        } 
+   
+        public class True : Expr 
+        { 
+            public override bool Eval() { return true; } 
+        } 
+        public class False : Expr 
+        { 
+            public override bool Eval() { return true; } 
+        } 
+  
+        public class Base : Expr 
+        { 
+            private readonly bool value; 
+            public Base(bool value) { this.value = value; } 
+            public override bool Eval() 
+            { 
+                return value;  
+            }
+        }
+        public class And : Expr 
+        { 
+            private readonly Expr expr1; 
+            private readonly Expr expr2; 
+            public And(Expr expr1, Expr expr2)  
+            { 
+              this.expr1 = expr1; 
+              this.expr2 = expr2; 
+            } 
+            public override bool Eval() 
+            { 
+              return expr1.Eval() && expr2.Eval(); 
+            } 
+        } 
+        public class Or : Expr 
+        { 
+            private readonly Expr expr1; 
+            private readonly Expr expr2; 
+            public Or(Expr expr1, Expr expr2)  
+            { 
+              this.expr1 = expr1; 
+              this.expr2 = expr2; 
+            } 
+            public override bool Eval() 
+            { 
+              return expr1.Eval() || expr2.Eval(); 
+            } 
+        }
+        public class Not : Expr
+        {
+            private readonly Expr expr;
+            public Not(Expr expr)
+            {
+                this.expr = expr;
+            }
+            public override bool Eval()
+            {
+                return !expr.Eval();
+            }
+        }
+
+        #endregion
+
+        #region Example 3
+
+        public List<int> Destutter(List<int> list)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] != list[i + 1])
+                    result.Add(list[i]);
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Example 4
+
+        public enum ConnState
+        {
+            Connecting,
+            Connected,
+            Disconnected 
+        }
+        public class ConnInfo
+        {
+            private readonly ConnState state;
+            private readonly IPAddress address;
+            private readonly DateTime? lastPing;
+            private readonly int? lastPingId;
+            private readonly string sessionId;
+            private readonly DateTime? whenInitiated;
+            private readonly DateTime? whenDisconnected;
+
+            public ConnInfo(ConnState state, IPAddress address, DateTime? lastPing,
+                            int? lastPingId, string sessionId, DateTime? whenInitiated, DateTime? whenDisconnected)
+            {
+                this.state = state;
+                this.address = address;
+                this.lastPing = lastPing;
+                this.lastPingId = lastPingId;
+                this.sessionId = sessionId;
+                this.whenInitiated = whenInitiated;
+                this.whenDisconnected = whenDisconnected;
+            }
+
+            public ConnState State { get { return this.state; } }
+            public IPAddress Address { get { return this.address; } }
+            public DateTime? LastPing { get { return this.lastPing; } }
+            public int? LastPingId { get { return this.lastPingId; } }
+            public string SessionId { get { return this.sessionId; } }
+            public DateTime? WhenInitiated { get { return this.whenInitiated; } }
+            public DateTime? WhenDisconnected { get { return this.whenDisconnected; } }
+        }
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ fsharp-for-the-masses/Playground.fsx
@@ -0,0 +1,194 @@
+﻿// Example 0
+
+type Customer = { Name : string; Age : int }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Example 1
+
+let map f (x,y,z) = 
+   (f x, f y, f z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Example 2
+
+type Expr = | True 
+            | False 
+            | And  of  Expr * Expr
+            | Or   of  Expr * Expr
+            | Not  of  Expr
+            | Base of  bool
+ 
+let  rec eval expr  = 
+   match expr with 
+   | True  -> true 
+   | False -> false 
+   | Base v  -> v
+   | And  (x,y) -> eval x && eval y  
+   | Or  (x,y)  -> eval x || eval y 
+   | Not  x     -> not (eval x) 
+
+
+
+
+
+
+
+
+
+
+// Example 3
+
+let rec destutter l = 
+  match l with 
+  | []             -> [] 
+  | x :: y :: rest -> 
+    if  x = y then destutter (y :: rest) 
+    else  x :: destutter (y :: rest)
+
+
+
+
+
+
+
+
+
+
+
+// Example 4
+open System
+open System.Net
+
+type ConnState = 
+| Connecting 
+| Connected 
+| Disconnected 
+ 
+type ConnInfo = { 
+  State:                   ConnState; 
+  Server:                  System.Net.IPAddress;
+  LastPing:                option<DateTime>; 
+  LastPingId:              option<int>; 
+  SessionId:               string; 
+  WhenInitiated:           option<DateTime>; 
+  WhenDisconnected:        option<DateTime>; 
+}
+
+
+
+
+// Final Example
+
+open System.IO 
+open System.Text.RegularExpressions
+
+// Download big text
+let webClient = new WebClient()
+let text = webClient.DownloadString(Uri("http://norvig.com/big.txt"))
+
+
+// frequency count 
+let NWORDS = 
+    text |> (Regex "[a-zA-Z]+").Matches |> Seq.cast 
+    |> Seq.map (fun (m:Match) -> m.Value.ToLower()) |> Seq.countBy id |> Map.ofSeq
+
+let isKnown word = NWORDS.ContainsKey word 
+
+/// Compute the 1-character edits of the word
+let edits1 (word: string) = 
+    let splits = [for i in 0 .. word.Length do yield (word.[0..i-1], word.[i..])]
+    let deletes = [for a, b in splits do if b <> "" then yield a + b.[1..]]
+    let transposes = [for a, b in splits do if b.Length > 1 then yield a + string b.[1] + string b.[0] + b.[2..]]
+    let replaces = [for a, b in splits do for c in 'a'..'z' do if b <> "" then yield a + string c + b.[1..]]
+    let inserts = [for a, b in splits do for c in 'a'..'z' do yield a + string c + b]
+    deletes @ transposes @ replaces @ inserts |> Set.ofList
+
+edits1 "speling"
+edits1 "pgantom"
+
+/// Compute the 1-character edits of the word which are actually words
+let knownEdits1 word = 
+    let result = [for w in edits1 word do if Map.containsKey w NWORDS then yield w] |> Set.ofList
+    if result.IsEmpty then None else Some result 
+
+knownEdits1 "fantom"
+knownEdits1 "pgantom"
+
+/// Compute the 2-character edits of the word which are actually words
+let knownEdits2 word = 
+    let result = [for e1 in edits1 word do for e2 in edits1 e1 do if Map.containsKey e2 NWORDS then yield e2] |> Set.ofList
+    if result.IsEmpty then None else Some result 
+
+knownEdits2 "pgantom"
+knownEdits2 "quyck"
+
+
+/// Find the best correction for a word, preferring 0-edit, over 1-edit, over 
+/// 2-edit, and sorting by frequency.
+let findBestCorrection (word: string) = 
+    let words = 
+        if isKnown word then Set.ofList [word] 
+        else 
+            match knownEdits1 word with
+            | Some words -> words
+            | None ->
+            match knownEdits2 word with
+            | Some words -> words
+            | None -> Set.ofList [word]
+
+    words |> Seq.sortBy (fun w -> -NWORDS.[w]) |> Seq.head
+
+// Examples
+findBestCorrection "speling"
+findBestCorrection "korrecter"
+findBestCorrection "fantom"
+findBestCorrection "pgantom"
